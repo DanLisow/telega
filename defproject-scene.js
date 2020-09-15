4a7f8fc3b99a3d3ba075d6bb4708e9f158adbc3c
DanLisow <56221679+DanLisow@users.noreply.github.com>
2020-09-15 20:45:29 +0500

    Add files via upload

---
+++ defproject-scene.js
@@ -0,0 +1,101 @@
+const { Telegraf } = require("telegraf");
+const { Markup } = Telegraf;
+const Composer = require("telegraf/composer");
+const Stage = require("telegraf/stage");
+const WizardScene = require("telegraf/scenes/wizard");
+const mysql = require("mysql");
+const { leave } = Stage;
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "practice",
+});
+
+connection.query("SELECT * FROM `schedule`", (err, res) => console.log(err));
+
+function defprojectSceneGenerate() {
+  const stepHandler = new Composer();
+
+  const defproject = new WizardScene(
+    "defproject",
+    (ctx) => {
+      ctx.reply(
+        "1. Выберите направление: ",
+        Markup.inlineKeyboard([
+          [Markup.callbackButton("application", "application")],
+          [Markup.callbackButton("web", "web")],
+          [Markup.callbackButton("ai/ml", "aiml")],
+          [Markup.callbackButton("smm", "smm")],
+        ]).extra()
+      );
+      ctx.wizard.next();
+    },
+    stepHandler.on("callback_query", (ctx) => {
+      ctx.wizard.state.data = ctx.update.callback_query.data;
+      ctx.reply("2. Напишите название команды: ");
+      ctx.wizard.state.flag = true;
+      ctx.wizard.next();
+    }),
+    (ctx) => {
+      if (ctx.wizard.state.flag) {
+        ctx.wizard.state.team = ctx.message.text;
+      }
+      ctx.reply("3. Напишите желаемое время защиты (Формат - 00:00)");
+      ctx.wizard.next();
+    },
+    (ctx) => {
+      ctx.wizard.state.time = ctx.message.text.replace(" ", "");
+      const data = ctx.wizard.state.data;
+      let time = ctx.wizard.state.time;
+      const regexp = /\d{2}:\d{2}/;
+
+      if (!regexp.test(ctx.wizard.state.time)) {
+        ctx.reply(
+          "Ошибка в формате времени 😾,\nвведите любой символ для продолжения"
+        );
+        ctx.wizard.selectStep(2);
+        ctx.wizard.state.flag = false;
+      } else {
+        connection.query(
+          `SELECT time FROM schedule WHERE data = '${data}'`,
+          (err, res) => {
+            console.log(res);
+            if (res.length == 0) {
+              ctx.reply("Ответ записан!");
+
+              connection.connect(() => {
+                connection.query(
+                  `INSERT INTO schedule (id, team, data, time) VALUES (NULL, '${ctx.wizard.state.team}', '${ctx.wizard.state.data}', '${ctx.wizard.state.time}')`
+                );
+                connection.query("SET SESSION wait_timeout = 604800");
+              });
+              ctx.scene.leave();
+              console.log(ctx.wizard.state);
+            } else if (res.some((elem) => elem.time == time)) {
+              ctx.replyWithHTML(
+                `К сожалению, это время уже занято 😾\n<b>Время должно отличаться на 15 минут</b>\n❗️ Или проверьте на https://site.ru, что вы ранее не записывались на данное время\n\n=========================\n\nВведите любой символ для продолжения`
+              );
+              ctx.wizard.selectStep(2);
+              ctx.wizard.state.flag = false;
+            } else {
+              ctx.reply("Ответ записан!");
+
+              connection.query(
+                `INSERT INTO schedule (id, team, data, time) VALUES (NULL, '${ctx.wizard.state.team}', '${ctx.wizard.state.data}', '${ctx.wizard.state.time}')`
+              );
+              connection.query("SET SESSION wait_timeout = 604800");
+              ctx.scene.leave();
+              console.log(ctx.wizard.state);
+            }
+          }
+        );
+      }
+    }
+  );
+
+  return defproject;
+}
+
+module.exports = defprojectSceneGenerate;
---
+++ index.js
@@ -0,0 +1,78 @@
+const { Telegraf } = require("telegraf");
+const Keyboard = require("telegraf-keyboard");
+const session = require("telegraf/session");
+const Stage = require("telegraf/stage");
+const Scene = require("telegraf/scenes/base");
+const fetch = require("node-fetch");
+const { leave } = Stage;
+const defprojectSceneGenerate = require("./defproject-scene");
+const faqSceneGenerate = require("./faq-scene");
+const documentsSceneGenerate = require("./documents-scene");
+const contactsSceneGenerate = require("./contacts-scene");
+const cancelprojectSceneGenerate = require("./cancelproject-scene");
+const checkprojectSceneGenerate = require("./checkproject-scene");
+
+const TOKEN = "1394592988:AAEGt-VkIR1nMPXbZwTGqi22MpF8b3zipdo";
+const bot = new Telegraf(TOKEN);
+
+const mainKeyboard = new Keyboard();
+mainKeyboard
+  .add("Документы")
+  .add("Контакты", "FAQ")
+  .add("Записаться на защиту", "Отменить запись")
+  .add("Посмотреть время записи");
+
+const defproject = defprojectSceneGenerate();
+const faq = faqSceneGenerate();
+const documents = documentsSceneGenerate();
+const contacts = contactsSceneGenerate();
+const cancel = cancelprojectSceneGenerate();
+const check = checkprojectSceneGenerate();
+
+const stage = new Stage([documents, contacts, faq, defproject, cancel, check]);
+
+bot.use(session());
+bot.use(stage.middleware());
+
+require("http")
+  .createServer(bot.webhookCallback("https://server101.hosting.reg.ru"))
+  .listen(8080);
+
+bot.start((ctx) =>
+  ctx.reply(
+    "Привет! Я бот-помощник\nВыбери,что тебе нужно",
+    mainKeyboard.draw()
+  ),
+  fetch('https://прокомпетенции.рф/api/v1/Project/GetCustomers')
+    .then(resp => {
+      console.log(resp);
+    })
+);
+
+
+
+bot.hears("Документы", (ctx) => {
+  ctx.scene.enter("documents");
+});
+
+bot.hears("Контакты", (ctx) => {
+  ctx.scene.enter("contacts");
+});
+
+bot.hears("FAQ", (ctx) => {
+  ctx.scene.enter("faq");
+});
+
+bot.hears("Записаться на защиту", (ctx) => {
+  ctx.scene.enter("defproject");
+});
+
+bot.hears("Отменить запись", (ctx) => {
+  ctx.scene.enter("cancelproject");
+});
+
+bot.hears("Посмотреть время записи", (ctx) => {
+  ctx.scene.enter("checkproject");
+});
+
+bot.launch();
